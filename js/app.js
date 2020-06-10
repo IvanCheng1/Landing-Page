e6fde29b93bab7f7b3aab5c3780ad4fd4d7506f5
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-10 12:46:25 +0100

    nav bar

--- js/app.js
+++ js/app.js
@@ -18,14 +18,33 @@
  * 
  */
 
+const nav_sections = document.getElementsByTagName('section')
+const navbar = document.querySelector('#navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  */
+function buildNavBar(sections) {
+    for (const section of sections) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
 
+        const sectionName = section.getAttribute('data-nav');
+        const sectionId = section.getAttribute('id');
 
+        a.href = "#" + sectionId;
+
+        a.appendChild(document.createTextNode(sectionName));
+        li.appendChild(a);
+        navbar.appendChild(li);
+    }
+}
+
+for (const section of nav_sections) {
+    console.log(section.getAttribute('data-nav'));
+}
 
 /**
  * End Helper Functions
@@ -35,8 +54,11 @@
 
 // build the nav
 
+buildNavBar(nav_sections)
 
 // Add class 'active' to section when near top of viewport
+
+
 
 
 // Scroll to anchor ID using scrollTO event
